308d06f5b2f88d1c553eb54ea90d8584946977c8
btthanhk4 <thanhbeo2k4@gmail.com>
2025-05-09 21:24:49 +0700

    Buoi 1

---
+++ Buoi 1/Demo01_First_Project/src/com/data/Program.java
@@ -0,0 +1,17 @@
+package com.data;
+
+public class Program {
+    public static void main(String[] args) {
+        //lệnh in
+        System.out.println(10);
+        System.out.println("Hello World!");
+
+        int age = 18;
+        System.out.println(age);
+        String name = "Nguyen Ngoc";
+        System.out.println(name);
+        boolean isRich = true;
+        System.out.println(isRich);
+
+    }
+}
